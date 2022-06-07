a5e57db8476cf82a809456584df2fabc94ca78bb
MaximMarinov <maximmarinovv@gmail.com>
2022-06-07 23:29:09 +0300

    Added details component; Added _redirect file for Netlify

--- dog-adoption-center/src/app/app-routing.module.ts
+++ dog-adoption-center/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './feature/dogs/details/details.component';
 import { DogsListComponent } from './feature/dogs/dogs-list/dogs-list.component';
 import { HomeComponent } from './feature/pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'breeds', component: DogsListComponent},
+  {path: 'details/:id', component: DetailsComponent},
 ];
 
 @NgModule({
--- dog-adoption-center/src/app/feature/feature.module.ts
+++ dog-adoption-center/src/app/feature/feature.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { DogsListComponent } from './dogs/dogs-list/dogs-list.component';
 import { RouterModule } from '@angular/router';
+import { DetailsComponent } from './dogs/details/details.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DogsListComponent
+    DogsListComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    InfiniteScrollModule
   ],
   exports: [
     HomeComponent,
-    DogsListComponent
+    DogsListComponent,
+    DetailsComponent
   ]
 })
 export class FeatureModule { }
