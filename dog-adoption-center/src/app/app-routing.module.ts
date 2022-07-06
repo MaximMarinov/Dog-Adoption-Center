32af09da2aa26165ed25ea489f1166d4e9406d97
MaximMarinov <maximmarinovv@gmail.com>
2022-07-06 23:16:19 +0300

    Added route guards

--- dog-adoption-center/src/app/app-routing.module.ts
+++ dog-adoption-center/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { LoggedGuard } from './core/guards/logged.guard';
 import { DetailsComponent } from './feature/dogs/details/details.component';
 import { DogsListComponent } from './feature/dogs/dogs-list/dogs-list.component';
 import { HomeComponent } from './feature/pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'breeds', component: DogsListComponent},
-  {path: 'details/:id', component: DetailsComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'breeds', canActivate:[AuthGuard], component: DogsListComponent},
+  {path: 'details/:id', canActivate:[AuthGuard], component: DetailsComponent},
+  {path: 'register', canActivate:[LoggedGuard], component: RegisterComponent},
+  {path: 'login', canActivate:[LoggedGuard], component: LoginComponent},
 ];
 
 @NgModule({
