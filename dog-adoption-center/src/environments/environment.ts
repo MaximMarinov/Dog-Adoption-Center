7b16f8ad823a01947448b3856412fba89afe38c9
MaximMarinov <maximmarinovv@gmail.com>
2022-07-07 01:30:24 +0300

    minor changes

--- dog-adoption-center/src/environments/environment.ts
+++ dog-adoption-center/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   BASE_URL: 'https://api.thedogapi.com/v1',
-  AUTH_URL: 'https://adoption-center-maksim-marinov.herokuapp.com/'
+  AUTH_URL: 'https://adoption-center-maksim-marinov.herokuapp.com'
 };
 
 /*
